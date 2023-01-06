81934de28f54cb30c3272822e3deb6930406b38f
pna70 <pna70@icloud.com>
2023-01-06 19:22:39 +0000

    Refactored utf character generation into UTFEncoder class

--- Invaders/Program.cs
+++ Invaders/Program.cs
@@ -6,7 +6,7 @@ using System.Runtime.Intrinsics.X86;
 using System.Security;
 using System.Security.Cryptography;
 using System.Text;
-
+using Invaders;
 
 Console.OutputEncoding = Encoding.UTF8;
 
@@ -31,78 +31,6 @@ Console.WriteLine(invader + "  ;  " + invader2);
 //1f499 (blue heart)
 
 
-string? GenerateUTF8String(uint? cp)
-{
-    byte[]? bytes = null;
-
-    // Strip bit sequences for leading and trailing utf8 bytes and bitwise-or bit sequences into utf8 byte sequence
-    switch (cp)
-    {
-        case <= 0x7f:
-            //0xxxxxxx
-            bytes = new byte[] { (byte)cp };
-            break;
-
-        case >= 0x80 and <= 0x7ff:
-            //110xxxxx	10xxxxxx
-            bytes = new byte[] {
-                (byte)(0xC0 | (0x1f & cp >> 6)),
-                (byte)(0x80 | (0x3f & cp))
-            };
-            break;
-
-        case >= 0x800 and <= 0xffff:
-            //1110xxxx	10xxxxxx	10xxxxxx
-            bytes = new byte[] {
-                (byte)(0xe0 | (0x0f & cp >> 12)),
-                (byte)(0x80 | (0x3f & cp >> 6)),
-                (byte)(0x80 | (0x3f & cp))
-            };
-            break;
-
-        case >= 0x10000 and <= 0x10ffff:
-            //11110xxx	10xxxxxx	10xxxxxx	10xxxxxx
-            bytes = new byte[] {
-                (byte)(0xf0 | (0x07 & cp >> 18)),
-                (byte)(0x80 | (0x3f & cp >> 12)),
-                (byte)(0x80 | (0x3f & cp >> 6)),
-                (byte)(0x80 | (0x3f & cp))
-            };
-            break;
-    }
-
-    if (bytes is byte[])
-        return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
-    else
-        return null;
-}
-
-string? GenerateUTF16String(uint? cp)
-{
-    byte[]? bytes = null;
-
-    switch (cp)
-    {
-        case >= 0x0 and <= 0xffff:
-            bytes = new byte[] { (byte)(cp & 0xff), (byte)(cp >> 8) };
-            break;
-
-        case >= 0x10000 and <= 0x10FFFF:
-            cp -= 0x10000;
-            ushort u0 = (ushort)(0xD800 | (cp >> 10));
-            ushort u1 = (ushort)(0xDC00 | (cp & 0x3FF));
-
-            bytes = new byte[] { (byte)(u0 & 0xff), (byte)(u0 >> 8 & 0xff), (byte)(u1 & 0xff), (byte)(u1 >> 8 & 0xff) };
-            break;
-    }
-
-    if (bytes is byte[])
-        return Encoding.Unicode.GetString(bytes, 0, bytes.Length);
-    else
-        return null;
-}
-
-
 // Test
 bool quit = false;
 
@@ -125,8 +53,8 @@ while(!quit)
         Console.WriteLine("*** " + e.Message);
     }
 
-    string? utf8String = GenerateUTF8String(codePoint);
-    string? utf16String = GenerateUTF16String(codePoint);
+    string? utf8String = UTFEncoder.GenerateUTF8String(codePoint);
+    string? utf16String = UTFEncoder.GenerateUTF16String(codePoint);
 
     if (utf8String is string str8)
         Console.WriteLine("UTF8 string is {0}", str8);
---
+++ Invaders/UTFEncoder.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invaders
+{
+
+    internal interface CharacterEncoder
+    {
+        abstract static string? GenerateUTF8String(uint? cp);
+        abstract static string? GenerateUTF16String(uint? cp);
+    }
+
+    internal class UTFEncoder : CharacterEncoder
+    {
+        public static string? GenerateUTF8String(uint? cp)
+        {
+            byte[]? bytes = null;
+
+            // Strip bit sequences for leading and trailing utf8 bytes and bitwise-or bit sequences into utf8 byte sequence
+            switch (cp)
+            {
+                case <= 0x7f:
+                    //0xxxxxxx
+                    bytes = new byte[] { (byte)cp };
+                    break;
+
+                case >= 0x80 and <= 0x7ff:
+                    //110xxxxx	10xxxxxx
+                    bytes = new byte[] {
+                (byte)(0xC0 | (0x1f & cp >> 6)),
+                (byte)(0x80 | (0x3f & cp))
+            };
+                    break;
+
+                case >= 0x800 and <= 0xffff:
+                    //1110xxxx	10xxxxxx	10xxxxxx
+                    bytes = new byte[] {
+                (byte)(0xe0 | (0x0f & cp >> 12)),
+                (byte)(0x80 | (0x3f & cp >> 6)),
+                (byte)(0x80 | (0x3f & cp))
+            };
+                    break;
+
+                case >= 0x10000 and <= 0x10ffff:
+                    //11110xxx	10xxxxxx	10xxxxxx	10xxxxxx
+                    bytes = new byte[] {
+                (byte)(0xf0 | (0x07 & cp >> 18)),
+                (byte)(0x80 | (0x3f & cp >> 12)),
+                (byte)(0x80 | (0x3f & cp >> 6)),
+                (byte)(0x80 | (0x3f & cp))
+            };
+                    break;
+            }
+
+            if (bytes is byte[])
+                return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            else
+                return null;
+        }
+
+        public static string? GenerateUTF16String(uint? cp)
+        {
+            byte[]? bytes = null;
+
+            switch (cp)
+            {
+                case >= 0x0 and <= 0xffff:
+                    bytes = new byte[] { (byte)(cp & 0xff), (byte)(cp >> 8) };
+                    break;
+
+                case >= 0x10000 and <= 0x10FFFF:
+                    cp -= 0x10000;
+                    ushort u0 = (ushort)(0xD800 | (cp >> 10));
+                    ushort u1 = (ushort)(0xDC00 | (cp & 0x3FF));
+
+                    bytes = new byte[] { (byte)(u0 & 0xff), (byte)(u0 >> 8 & 0xff), (byte)(u1 & 0xff), (byte)(u1 >> 8 & 0xff) };
+                    break;
+            }
+
+            if (bytes is byte[])
+                return Encoding.Unicode.GetString(bytes, 0, bytes.Length);
+            else
+                return null;
+        }
+    }
+}
